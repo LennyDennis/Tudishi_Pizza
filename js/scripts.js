84f33329491954c94c989cfc79efc50040f29c6d
LennyDennis <lennydennis@gmail.com>
2019-12-16 14:53:17 +0300

    adds the add toppings functionality on clicking the add toppings button

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  $(".delivery").hide();
+  $(".yes").hide();
+  $(".no").hide();
+  $(".delivery-info h4").hide();
+
+    $("#addtopping").click(function(event) {
+      event.preventDefault();
+      $(".toppings").append( '<select id="topping1" class="input">'+
+                                 '<option value="" hidden disabled selected value>Pizza topping....</option>'+
+                                 '<option value="300">Pepperoni</option>'+
+                                 '<option value="2500">Mushrooms</option>'+
+                                 '<option value="200">Sausage</option>'+
+                                 '<option value="200">Onions</option>'+
+                                 '<option value="200">Bacons</option>'+
+                                 '<option value="200">Extra Cheese</option>'+
+                             '</select>');
+    });
