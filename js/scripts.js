6741d903c8d6eeacadaf04f0028a3a0aaf04a304
LennyDennis <lennydennis@gmail.com>
2019-12-16 15:02:02 +0300

    adds the delivery and alert  functionality

--- js/scripts.js
+++ js/scripts.js
@@ -43,3 +43,51 @@ $(document).ready(function(){
       var grandTotal = 0;
 
       $('table#tablecart').append('<tr>'+'<td>'+ $("#size option:selected").text()+'</td>'+ '<td id="quantity">'+quantity+'</td>' + '<td id="price">'+cost+'</td>' +'<td id="total">'+total+'</td>'+'</tr>');
+
+      $(".checkout").click(function() {
+        $(".checkout").hide();
+        $(".delivery").show();
+        $("#yes").hide();
+        $("#no").hide();
+        $("#totalcost").show();
+        $("#delivercost").show();
+        $(".yes-button").show();
+        $(".no-button").show();
+        $(".address").hide();
+
+        grandTotal = grandTotal + total;
+        $("p span").html(grandTotal);
+      });   
+      $(".yes-button").click(function() {
+      $("#delivercost").hide();
+      $("#no").hide();
+      $("#yes").hide();
+      $(".address").show();
+      $(".yes-button").hide();
+      $(".no-button").hide();
+      $("#totalcost span").html(grandTotal + 300);
+    });
+      $(".no-button").click(function() {
+      $("#delivercost").hide();
+      $("#yes").hide();
+      $("#no").show();
+      $(".yes-button").hide();
+      $(".no-button").hide();
+      $("#totalcost span").html(grandTotal);
+    });
+    $("#addaddress").click(function() {
+    var address = $(".address input").val();
+    $("#yes").show();
+    $(".address").hide();
+    $("#yes span").html(address);
+  });
+  $(".refresh").click(function(event){
+    $('form').each(function(){
+      this.reset();
+      $(".delivery").hide();
+      $("#tablecart tbody tr").remove(); 
+
+    });
+  });
+  });
+});
